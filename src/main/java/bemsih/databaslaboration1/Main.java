36c8ed3889b0b07ce62d1937aa02d8899de2a638
mahaf03 <mahsih@kth.se>
2024-12-08 11:39:17 +0100

    Main now working

--- src/main/java/bemsih/databaslaboration1/Main.java
+++ src/main/java/bemsih/databaslaboration1/Main.java
@@ -14,25 +14,25 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             boolean running = true;
 
-//            while (running) {
-//                System.out.println("\nVälj ett alternativ:");
-//                System.out.println("1. Sök efter en bok baserat på titel");
-//                System.out.println("2. Sök efter en bok baserat på författarnamn");
-//                System.out.println("3. Avsluta");
-//
-//                int choice = scanner.nextInt();
-//                scanner.nextLine(); // Konsumera radbrytningen
-//
-//                switch (choice) {
-//                    case 1 -> searchByTitle(connection, scanner);
-//                    case 2 -> searchByAuthor(connection, scanner);
-//                    case 3 -> {
-//                        System.out.println("Avslutar programmet.");
-//                        running = false;
-//                    }
-//                    default -> System.out.println("Ogiltigt val. Försök igen.");
-//                }
-//            }
+            while (running) {
+                System.out.println("\nVälj ett alternativ:");
+                System.out.println("1. Sök efter en bok baserat på titel");
+                System.out.println("2. Sök efter en bok baserat på författarnamn");
+                System.out.println("3. Avsluta");
+
+                int choice = scanner.nextInt();
+                scanner.nextLine(); // Konsumera radbrytningen
+
+                switch (choice) {
+                    case 1 -> searchByTitle(connection, scanner);
+                    case 2 -> searchByAuthor(connection, scanner);
+                    case 3 -> {
+                        System.out.println("Avslutar programmet.");
+                        running = false;
+                    }
+                    default -> System.out.println("Ogiltigt val. Försök igen.");
+                }
+            }
             scanner.close();
         } catch (SQLException e) {
             System.out.println("Anslutning misslyckades.");
